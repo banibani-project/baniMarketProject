24a128299303f7f747f4cd38ace9e22fd6081c11
hong <hong@DESKTOP-PEE814T>
2021-10-14 20:27:28 +0900

    First Commit 2021/10/14 - 최초커밋!

---
+++ FPS/src/main/java/com/banibani/FpsApplication.java
@@ -0,0 +1,13 @@
+package com.banibani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpsApplication.class, args);
+	}
+
+}
---
+++ FPS/src/test/java/com/banibani/FpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banibani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
