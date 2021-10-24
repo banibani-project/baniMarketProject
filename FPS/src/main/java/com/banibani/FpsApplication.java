b3fc0b6a9a086fad500deda9e31ab6aa9104b1e1
nice <nice@119.207.203.183>
2021-10-24 16:22:28 +0900

    jsr new branch add

--- FPS/src/main/java/com/banibani/FpsApplication.java
+++ FPS/src/main/java/com/banibani/FpsApplication.java
@@ -1,7 +1,10 @@
 package com.banibani;
 
+import javax.servlet.http.HttpServlet;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class FpsApplication {
@@ -9,5 +12,4 @@ public class FpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FpsApplication.class, args);
 	}
-
 }
