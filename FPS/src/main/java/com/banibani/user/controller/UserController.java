66786afa03798a75bcbe21f8df8e320282e2d62f
wogml3270 <wogml3270@naver.com>
2021-11-16 22:13:07 +0900

    .

--- FPS/src/main/java/com/banibani/user/controller/UserController.java
+++ FPS/src/main/java/com/banibani/user/controller/UserController.java
@@ -1,18 +1,14 @@
 package com.banibani.user.controller;
 
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.banibani.user.service.UserSerivce;
-import com.banibani.user.vo.UserVo;
+import com.banibani.user.service.UserService;
 
 
 @RequestMapping(value="/user")
 public class UserController {
 
-	UserSerivce userService;
+	UserService userService;
 
 	@RequestMapping(value="/openUserCreate.do")
 	public String openUserCreate() {
@@ -20,19 +16,14 @@ public class UserController {
 		return "";
 	}
 
-	/**
-	 * 작성일 : 2021 - 10 - 31
-	 * 작성자 : JJH
-	 * 바라보는 데이터 : openUserCreatePage.jsp
-	 * 파라미터 데이터 : [ userId : value, userPassword : vale, userName : value, userPhone : value, userAge : value, userAddress : value, userTypeCode : value ]
-	 */
+	/*
 	@ResponseBody
-	@RequestMapping(value="/UserIdVlaueCheck")
-	public ModelMap UserIdVlaueCheck (@RequestBody UserVo userVo) {
+	@RequestMapping(value="/open")
+	public ModelMap (@RequestBody request, DefaultVo defaultVo) {
 
-		return userService.UserIdVlaueCheck(userVo);
+		return userService.(defaultVo);
 
 	}
-
+	*/
 
 }
