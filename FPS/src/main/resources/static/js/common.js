e19e3ff435cce8705ecc09d92bd6ee51c2252ffb
rlorloc47 <89571328+rlorloc47@users.noreply.github.com>
2021-12-19 01:15:06 +0900

    메인 페이지 이미지 보이게 변경 및 채팅 이력/호감 이력 표시

--- FPS/src/main/resources/static/js/common.js
+++ FPS/src/main/resources/static/js/common.js
@@ -3,3 +3,11 @@
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function isEmpty(value)	{ 
+	if( value == "" || value == null || value == undefined || ( value != null && typeof value == "object" && !Object.keys(value).length ) ){
+		return true;
+	}else{
+		return false 
+	}
+};
